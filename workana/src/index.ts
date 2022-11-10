9ab9308d055a2b064e16daa254e54e6d7481169f
Luis Mendes <machadoluismendes@gmail.com>
2022-11-10 20:47:03 -0300

    Increment
    create react app
    reactJS 18
    typescript

--- workana/src/index.ts
+++ workana/src/index.ts
@@ -1,3 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+    <App />
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
+
 require("@babel/core").transform("code", {
   presets: ["@babel/preset-env"],
 });
@@ -18,12 +37,12 @@ server();
 // typescript 2305
 // import {Date} from 'typescript';
 
-import React from 'react';
-import * as ReactDOM from "react-dom/client";
+// import React from 'react';
+// import * as ReactDOM from "react-dom/client";
 import createRoot from 'react-dom/client';
 // import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import {applyPolyfills, defineCustomElements} from 'h8k-components/loader';
 
