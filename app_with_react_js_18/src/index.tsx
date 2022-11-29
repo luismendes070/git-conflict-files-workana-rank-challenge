d21eeda37907376bebe553921a924bcf05535bef
Luis Mendes <machadoluismendes@gmail.com>
2022-11-29 19:54:41 -0300

    Increment
    logrocket

--- app_with_react_js_18/src/index.tsx
+++ app_with_react_js_18/src/index.tsx
@@ -1,3 +1,7 @@
+import LogRocket from 'logrocket';
+import setupLogRocketReact from 'logrocket-react';
+
+// import LogRocket from 'logrocket';
 // component, filtercards,
 import { index } from './workana/index.js';
 
@@ -6,6 +10,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+LogRocket.init('izrzoo/hello-world');
+
+// This is an example script - don't forget to change it!
+LogRocket.identify('THE_USER_ID_IN_YOUR_APP', {
+  name: 'James Morrison',
+  email: 'jamesmorrison@example.com',
+
+  // Add your own custom user variables here, ie:
+  subscriptionType: 'pro'
+});
+
+// after calling LogRocket.init()
+setupLogRocketReact(LogRocket);
 
 index();
 
